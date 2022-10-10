f19153552adb05072dd81cd52daad3a935451a5f
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2022-10-10 16:15:54 +0200

    i

--- app.js
+++ app.js
@@ -2,36 +2,36 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const http = require("http");
-const childprocess = require("child_process");
 const { Server } = require("socket.io");
 const io = new Server(server);
 const server = http.createServer(app);
 
-childprocess.exec("npm install", (err, stdo, stderr) => {
-  if (err) throw err;
-  console.log(stdo, stderr);
-  function loadRoutes(callback) {
-    let files = fs.readdirSync(`${__dirname}/src/routes`);
-    files.forEach((f) => {
-      let n = f.split(".");
-      if (n[1] === ".js") {
-        app.use(`/${n[0]}`, require(`./src/routes/${f}`));
-        console.log(`Loading ${f}`);
-      }
-    });
+// childprocess.exec("npm install", (err, stdo, stderr) => {
+//   if (err) throw err;
+//   console.log(stdo, stderr);
+// });
 
-    callback();
-  }
-
-  io.on("connection", (socket) => {
-    console.log("a user connected");
+function loadRoutes(callback) {
+  let files = fs.readdirSync(`${__dirname}/src/routes`);
+  files.forEach((f) => {
+    let n = f.split(".");
+    if (n[1] === ".js") {
+      app.use(`/${n[0]}`, require(`${__dirname}/src/routes/${f}`));
+      console.log(`Loading ${f}`);
+    }
   });
 
-  loadRoutes(() => {});
+  callback();
+}
 
-  app.get("/", (req, res) => {
-    res.send("OK");
-  });
-
-  server.listen(8080 || process.env.PORT);
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
+
+loadRoutes(() => {});
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+server.listen(8080 || process.env.PORT);
