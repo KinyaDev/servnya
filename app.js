b89c0e7398a4d19a54e4888fe821b09109cecdd2
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2022-10-10 14:28:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const http = require("http");
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+function loadRoutes(callback) {
+  let files = fs.readdirSync(`${__dirname}/src/routes`);
+  files.forEach((f) => {
+    let n = f.split(".");
+    if (n[1] === ".js") {
+      app.use(`/${n[0]}`, require(`./src/routes/${f}`));
+      console.log(`Loading ${f}`);
+    }
+  });
+
+  callback();
+}
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+const server = http.createServer(app);
+
+loadRoutes(() => {});
+
+server.listen(8080 || process.env.PORT);
