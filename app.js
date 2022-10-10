de0ce67180c0e2ddebb16d6a037ac079a48f0fe8
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2022-10-10 18:52:24 +0200

    i

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://kinyadev.github.io/lang");
+  res.header("Access-Control-Allow-Origin", "https://kinyadev.github.io");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
