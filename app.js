02b05c051b7a526904fe1d04f8da9a755832d125
KinyaDev <114854571+KinyaDev@users.noreply.github.com>
2022-10-11 15:54:15 +0200

    Err fix

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express();
 
 const bodyParser = require("body-parser");
 
-app.use(bodyParser);
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://kinyadev.github.io");
   res.header(
